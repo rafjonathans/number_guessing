394d2043fcf6d56c50018956dd7e3a80f9994068
rafjonathans <rafael.jeremia@gmail.com>
2021-03-08 11:55:35 +0700

    Update game.py

--- game.py
+++ game.py
@@ -12,23 +12,23 @@ score = 100
 # Game
 if player_name in player_data["id"] and player_pass in player_data["password"]:
     # Game is on if ID and password input OK
-    while score > 0 or int(tebak) != angka:
-        while int(tebak) != angka:
-            print("Score saat ini " + str(score))
-            tebak = input("Tebak angka antara 1 - 20: ")
-            if tebak.strip().isdigit():
-                if int(tebak) < angka:
-                    print("Tebak lebih besar")
-                    score = score - 10
-                elif int(tebak) > angka:
-                    print("Tebak lebih kecil")
-                    score = score - 10
-                else:
-                    print("ANDA BENARRRR")
+    while score > 0 and int(tebak) != angka:
+        print("Score saat ini " + str(score))
+        tebak = input("Tebak angka antara 1 - 20: ")
+        if tebak.strip().isdigit():
+            if int(tebak) < angka:
+                print("Tebak lebih besar")
+                score = score - 10
+            elif int(tebak) > angka:
+                print("Tebak lebih kecil")
+                score = score - 10
             else:
-                score = score - 20
-                print("TEBAKNYA ANGKA WOY!")
-        break
+                print("ANDA BENARRRR")
+        else:
+            score = score - 20
+            print("TEBAKNYA ANGKA WOY!")
+            tebak = 0
+    
 
     ## Result
     ### Show
